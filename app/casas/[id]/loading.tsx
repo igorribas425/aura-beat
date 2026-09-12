30acc3a23196d0b614ee55dcd526ee1cf768147d
igorribas425 <igorribas425@gmail.com>
2026-09-12 13:15:47 -0300

    feat: add public artist and venue profiles

---
+++ app/casas/[id]/loading.tsx
@@ -0,0 +1,3 @@
+export default function LoadingVenueProfile() {
+  return <main className="flex min-h-[70vh] items-center justify-center bg-[#050507] text-zinc-400">Carregando perfil público…</main>;
+}
